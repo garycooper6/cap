8519c826c9008fe7838a115247ebf844a1494b58
Gary Cooper <garycooper6@hotmail.com>
2018-10-18 11:41:29 +0100

    Include semantic ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cap';
+  pageTitle = 'Commercial Application Portal';
 }
 
 const test = 'test';

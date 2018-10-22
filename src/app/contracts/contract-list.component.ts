fcf5b74ab6dcd34df2dd25a8a69bf01e9ab02709
Gary Cooper <garycooper6@hotmail.com>
2018-10-22 16:20:30 +0100

    Basic binding and pipes

--- src/app/contracts/contract-list.component.ts
+++ src/app/contracts/contract-list.component.ts
@@ -8,6 +8,10 @@ import { Component } from '@angular/core';
 
 export class ContractListComponent {
     pageTitle = 'Contract List';
+    imageWidth = 50;
+    imageMargin = 2;
+    showImage = false;
+    listFilter = 'station';
     contracts: any[] = [
         {
             'contractId': 1,
@@ -15,7 +19,8 @@ export class ContractListComponent {
             'details': 'Moorgate Tunnel cleaning',
             'status': 0,
             'tenderValue': 175819.51,
-            'startDate': '09/02/2015'
+            'startDate': '09/02/2015',
+            'imageUrl': 'https://openclipart.org/image/800px/svg_to_png/158773/Boton-correcto.png'
         },
         {
             'contractId': 2,
@@ -23,7 +28,12 @@ export class ContractListComponent {
             'details': 'Manningtree Station',
             'status': 0,
             'tenderValue': 1748529.80,
-            'startDate': '04/08/2015'
+            'startDate': '04/08/2015',
+            'imageUrl': 'https://openclipart.org/download/158779/Boton-mal.svg'
         }
     ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }
